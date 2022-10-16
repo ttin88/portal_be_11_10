e8ca84de6a9fe164fe7ce95130b03ffec3de3115
benit58383 <benit58383@gmail.com>
2022-10-16 11:53:55 +0700

    fix user

--- routes/users.js
+++ routes/users.js
@@ -28,69 +28,51 @@ let userSchema = mongoose.Schema({
   phone: {
     type: String,
   },
-  fullName: {
+  full_name: {
     type: String,
   },
-  citizenId: {
+  citizen_id: {
     type: String,
   },
-  userName: {
+  user_name: {
     type: String,
   },
   password: {
     type: String,
   },
-  smartOtp: {
+  smart_otp: {
     type: String,
   },
-  createdAt: {
+  created_at: {
     type: String,
   },
-  updatedAt: {
+  updated_at: {
     type: String,
   },
 });
 
 let UserModel = mongoose.model('users', userSchema);
 
-//MODEL
-
-// const { MongoClient } = require("mongodb");
-
-// const uri =
-//   "mongodb+srv://tech:tech123@cluster0.sn37e.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri);
-
-// client.connect();
-// console.log("connected!");
-
-// const db = client.db("portal");
-// const coll = db.collection("users");
-
-
 router.post("/create", async (req, res, next) => {
   try {
     let body = req.body
 
-    body.createdAt = moment().format('DD/MM/YYYY hh:mm:ss')
-    body.updatedAt = moment().format('DD/MM/YYYY hh:mm:ss')
+    body.created_at = moment().format('DD/MM/YYYY hh:mm:ss')
+    body.updated_at = moment().format('DD/MM/YYYY hh:mm:ss')
     await UserModel.create(body)
       .then((result) => {
         res.status(200).send(result);
-        // res.status(200).json({ messages: "create success", status: "200" })
       })
       .catch((err) => {
         res.status(400).send({
           message: err.message || "Error occurred while creating the user",
         });
-        // res.status(400).json({ messages: "created failure", status: "400" })
       })
   } catch (error) {
     console.log('---------error----------', error);
     res.status(400).send({
       message: err.message || "Error occurred while creating the user",
     });
-    // res.status(400).json({ error, messages: "created failure", status: "400" })
   }
 })
 
@@ -102,33 +84,30 @@ router.get("/", async (req, res, next) => {
         let result = users.map((user) => {
           let element = {};
           element["Id"] = user.id;
-          element["Tên ngân hàng"] = user.bankName;
+          element["Tên ngân hàng"] = user.bank_name;
           element["Số điện thoại"] = user.phone;
-          element["Họ và tên"] = user.fullName;
-          element["Chứng minh thư / CCCD"] = user.citizenId;
-          element["Tên tài khoản"] = user.userName;
+          element["Họ và tên"] = user.full_name;
+          element["Chứng minh thư / CCCD"] = user.citizen_id;
+          element["Tên tài khoản"] = user.user_name;
           element["Mật khẩu"] = user.password;
-          element["Mã otp"] = user.smartOtp;
-          element["Ngày tạo"] = user.createdAt;
+          element["Mã otp"] = user.smart_otp;
+          element["Ngày tạo"] = user.created_at;
 
           return element;
         })
 
         res.status(200).send(result);
-        // res.status(200).json({ result, messages: "get data success", status: "200" })
       })
       .catch((err) => {
         res.status(400).send({
           message: err.message || "get data failure",
         });
-        // res.status(400).json({ messages: "get data failure", status: "400" })
       })
   } catch (error) {
     console.log('---------error----------', error);
     res.status(400).send({
       message: err.message || "get data failure",
     });
-    // res.status(400).json({ messages: "get data failure", status: "400" })
   }
 })
 
