285de85b91a00e6bc4d2a07cd974c2aca0bd69e0
benit58383 <benit58383@gmail.com>
2022-10-16 08:53:15 +0700

    fix user

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,9 @@ var moment = require('moment');
 
 // const uri = "mongodb+srv://portal:portal@cluster0.h9szmnf.mongodb.net/?retryWrites=true&w=majority";
 // const uri = "mongodb+srv://portal:portal@portalcluster.1dumg2v.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://portal:portal@clusterok.xtjicvp.mongodb.net/ben_collection";
+// const uri = "mongodb+srv://portal:portal@clusterok.xtjicvp.mongodb.net/ben_collection";
+const uri = "mongodb+srv://portal:portal@clusterok.xtjicvp.mongodb.net/ben_collection"
+
 
 const functionConnect = async () => {
   try {
