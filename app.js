49d5bf033b989766be864c47999d620c6fa33579
benit58383 <benit58383@gmail.com>
2022-10-16 13:48:46 +0700

    fix user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var usersRouter = require('./routes/users');
 
@@ -17,6 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors('*'));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'POST,GET,LINK');
+// });
 
 app.use('/', usersRouter);
 
